5519f600f65bef405a2c94194a86b1f1ba9cecb2
Phearun <phearun.chhun@student.passerellesnumeriques.org>
2022-04-28 09:49:39 +0700

    merge

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // const PORT = 3000;
-// app.use(cors({ origin: '*' })); // To allow any origin
+app.use(cors({ origin: '*' })); // To allow any origin
 app.use(express.urlencoded())
 app.use(express.json());
 app.listen(PORT, () => {
