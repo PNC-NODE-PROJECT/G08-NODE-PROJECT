193c4a442d5a30783bff18104c6ce42a5bb88ecf
phandyphorn <90320702+phandyphorn@users.noreply.github.com>
2022-05-01 17:59:50 +0700

    #16 register frondend

--- routes/user_route.js
+++ routes/user_route.js
@@ -5,28 +5,28 @@ const mongoose = require('mongoose');
 // user route
 // get all the users 
 
-// router.get('/', (req, res) => {
-//     models_user.users_models.find()
-//         .then((result) => {
-//             res.send(result);
-//         })
-//         .catch((err) => {
-//             res.send(err)
-//             console.error(err)
-//         })
-// })
+router.get('/', (req, res) => {
+    models_user.users_models.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.send(err)
+            console.error(err)
+        })
+})
 
-// router.post("/login", (req, res) => {
-//     let user_data = req.body;
-//     models_user.users_models.find(user_data)
-//     .then((result) => {
-//         res.send(result)
-//     })
-//     .catch((err) => {
-//         res.send(err)
-//         console.log(err)
-//     })
-// })
+router.post("/login", (req, res) => {
+    let user_data = req.body;
+    models_user.users_models.find(user_data)
+    .then((result) => {
+        res.send(result)
+    })
+    .catch((err) => {
+        res.send(err)
+        console.log(err)
+    })
+})
 
 router.post("/create_user", (req, res) => {
     let user_register = req.body;
